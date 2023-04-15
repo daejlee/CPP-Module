03251d305efc7e3f0fe009861a3b6443cb9bbf01
Daejin Lee <daejlee@c1r7s1.42seoul.kr>
2023-04-15 16:20:49 +0900

    ex00 preped for exam

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -2,16 +2,20 @@
 
 int	main(void)
 {
-	ClapTrap	Bob("Bob");
+	std::cout << "-------Constructor / Destructor test start-------" << std::endl;
+	ClapTrap	Anonymous;
 	ClapTrap	Jim("Jim");
 	ClapTrap	ArtyJim = Jim;
+	std::cout << "-------Constructor / Destructor test end-------\n" << std::endl;
 
-	Bob.attack("Mutated Frog");
-	Bob.beRepaired(2);
-	for (int i = 0; i < 12; i++)
-		Bob.attack("Mutated Frog");
+	std::cout << "-------member function test start-------" << std::endl;
+	Anonymous.attack("Mutated Frog");
+	Anonymous.beRepaired(2);
+	for (int i = 0; i < 9; i++)
+		Anonymous.attack("Mutated Frog");
 	Jim.attack("Hydrarisk");
 	ArtyJim.attack("Hydrarisk");
+	std::cout << "-------member function test end-------\n" << std::endl;
 
 	return (0);
 }
