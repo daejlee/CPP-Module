81152c20ef3b3d4ed7a8f7e7a03f4d3f196a53f5
Daejin Lee <daejlee@c3r8s5.42seoul.kr>
2023-06-15 21:05:32 +0900

    module09 added

---
+++ module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange
+{
+private:
+    /* data */
+public:
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &obj);
+    operator=();
+    ~BitcoinExchange();
+};
+
+#endif
